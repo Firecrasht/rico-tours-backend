0f4a91d8f102ffe97d544b913035b3c0d9d0accf
Firecrasht <ricotourspv@gmail.com>
2025-06-10 23:55:44 -0600

    Delete routes directory

--- routes/camionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { crearCamion, obtenerCamiones } = require("../controllers/camionController");
-
-router.post("/", crearCamion);
-router.get("/", obtenerCamiones);
-
-module.exports = router;

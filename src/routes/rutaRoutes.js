83e828e8e90b99cd629bb803d1e11fc1ad82278b
Firecrasht <ricotourspv@gmail.com>
2025-06-10 21:56:52 -0600

    Delete src directory

--- src/routes/rutaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { crearRuta, obtenerRutas } = require("../controllers/rutaController");
-
-router.post("/", crearRuta);
-router.get("/", obtenerRutas);
-
-module.exports = router;

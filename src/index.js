61cd839d20894fb8c27fa09a5291be9f71fb4a73
Firecrasht <ricotourspv@gmail.com>
2025-06-13 12:31:55 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => res.send('Rico Tours API funcionando'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
